e951da74d2333ad9474eef09759a8287fbb3cdfc
Abdifatah Mohamed <abdi.m.official@gmail.com>
2024-02-07 14:32:06 -0800

    Assignment complete, output text file

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,7 +4,7 @@
 
 def sum(arr)
   # YOUR CODE HERE
-  arr.sum()
+  arr.sum
 end
 
 def max_2_sum(arr)
@@ -14,19 +14,17 @@ end
 
 def sum_to_n?(arr, number)
   # YOUR CODE HERE
-  combo_array = arr.combination(2).map{|a,b| a+b}
-  if arr.length < 2
-    return false
-  else 
-    combo_array.include?(number)
-  end 
+  combo_array = arr.combination(2).map { |a, b| a + b }
+  return false if arr.length < 2
+
+  combo_array.include?(number)
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
-  "Hello, " + name
+  'Hello, ' + name
 end
 
 def starts_with_consonant?(string)
@@ -36,7 +34,7 @@ end
 
 def binary_multiple_of_4?(string)
   # YOUR CODE HERE
-  string.match?(/\A[01]+\z/) and (string.to_i)%4 == 0
+  string.match?(/\A[01]+\z/) and string.to_i % 4 == 0
 end
 
 # Part 3
@@ -44,4 +42,24 @@ end
 # Object representing a book
 class BookInStock
   # YOUR CODE HERE
+
+  attr_reader :isbn, :price
+  def isbn=(new_isbn)
+    raise ArgumentError, 'empty ISBN' if new_isbn.empty?
+    @isbn = new_isbn
+  end
+
+  def price=(new_price)
+    raise ArgumentError, 'price is less than or equal to 0' if new_price <= 0
+    @price = new_price
+  end
+
+  def initialize(isbn, price)
+    self.isbn = isbn
+    self.price = price
+  end
+
+  def price_as_string
+    '$' + '%.2f' % price
+  end
 end
